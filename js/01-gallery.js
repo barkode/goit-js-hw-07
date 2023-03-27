20b83610cac27571709e350cdde614970132ccbf
barkode <konstantinks@gmail.com>
2023-03-27 23:13:43 +0100

    add gallery first

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = galleryItems.reduce(
+  (acc, { preview, original, description }) => {
+    acc += `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+       <img class="gallery__image"
+         src="${preview}"
+         data-source="${original}" alt="${description}"/></a></li>`;
+    return acc;
+  },
+  ""
+);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+gallery.addEventListener("click", openOriginImage);
+
+function openOriginImage(event) {
+  event.preventDefault();
+  const originalImagePath = event.target.dataset.source;
+  console.log(originalImagePath);
+  const instance =
+    basicLightbox.create(`<img src="${originalImagePath}" width="800" height="600">
+   `);
+  instance.show();
+}
